9210bd8044c3ed2389c84178f889c6da85732bea
lukeseo <lukeseo@gmail.com>
2013-01-16 00:56:15 -0800

    First from Ubuntu

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hellobaby::Application.config.secret_token = '78938f09bb4cf51d1f374f4d961cfaf8700eb0e137d032e9ec56b14cdf2c6bc8f4f2394e0daa9a360b8f06f52d7c6ab560cc7601eae8baaae49831b64db88c4a'
